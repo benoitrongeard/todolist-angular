c1493f87e2b4fa49213732623ca858d4fb893b49
Benoit Rongeard <benron@hotmail.fr>
2018-06-11 09:06:15 +0200

    :wrench: Adding tasks structure

---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActiveComponent } from './pages/active/active.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ActiveComponent]
+})
+export class TasksModule { }

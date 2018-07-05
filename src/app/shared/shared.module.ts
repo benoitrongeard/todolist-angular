741f2e617e9aaf4ef0f18396fe97aa4c26337906
dualshote <benron@hotmail.fr>
2018-07-05 13:13:12 +0200

    :sparkles: Adding shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+//Module that share all generic components
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    NavbarComponent
+  ]
+})
+export class SharedModule { }

2666af5d430c2093ee8b603cff2508014d434d7d
dualshote <benron@hotmail.fr>
2018-07-13 14:14:58 +0200

    :sparkles: Adding logout and toggleMenu functionality

--- src/app/ui/navbar/navbar.component.ts
+++ src/app/ui/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'td-navbar',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  private activeMobileMenu: boolean = false;
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    this.activeMobileMenu = !this.activeMobileMenu;
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
   }
 
 }

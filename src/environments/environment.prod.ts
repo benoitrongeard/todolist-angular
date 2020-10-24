2beb5d4eea3ddc07cb1509aae147e1868ff60685
Benoit Rongeard <benron@hotmail.fr>
2020-10-24 15:02:13 +0200

    Update backend API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "https://todolist-api.guillaumebriday.xyz/api/v1/",
-  broadcastingBaseUrl: 'https://todolist-api.guillaumebriday.xyz/',
+  api: "https://todolist-api.guillaumebriday.me/api/v1/",
+  broadcastingBaseUrl: 'https://todolist-api.guillaumebriday.me/',
   pusher_app_key: '711760424fa1664773d1'
 };
